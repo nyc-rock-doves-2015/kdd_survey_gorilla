c05cd661f8786a6de587d83b6b3984facba5b124
Dan Asselin <dasselin86@gmail.com>
2015-04-17 17:20:45 -0400

    Fill in each controller (except index) with the erb they will link to

--- source/app/controllers/survey.rb
+++ source/app/controllers/survey.rb
@@ -6,30 +6,30 @@ get '/surveys' do
   redirect '/'
 end
 
-get '/surveys/new'
-
+get '/surveys/new' do
+  erb :'surveys/new'
 end
 
-post '/surveys'
-
+post '/surveys' do
+  'create a new survey!'
 end
 
-get '/surveys/:id' |id|
-
+get '/surveys/:id' do |id|
+  erb :'surveys/show'
 end
 
-get '/surveys/:id/edit' |id|
-
+get '/surveys/:id/edit' do |id|
+  erb :'surveys/edit'
 end
 
-put '/surveys/:id' |id|
-
+put '/surveys/:id' do |id|
+  'edit a survey!!'
 end
 
-delete '/surveys/:id' |id|
-
+delete '/surveys/:id' do |id|
+  'delete the survey!!'
 end
 
-get '/surveys/:id/stats' |id|
-
+get '/surveys/:id/stats' do |id|
+  erb :'surveys/stats'
 end

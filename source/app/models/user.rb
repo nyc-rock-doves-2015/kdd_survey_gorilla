9fa660ec67a9f31b78667c48a2f4062d0d867965
Dan Asselin <dasselin86@gmail.com>
2015-04-17 19:36:41 -0400

    Add 'user has many answers' to user model

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   has_many :authored_surveys, class_name: "Survey"
   has_many :survey_users
   has_many :taken_surveys, through: :survey_users, source: :survey
+  has_many :answers
 end

f4d52f520ace3f60b6229343c9dc6c9fff188488
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-17 15:42:16 -0400

    Working migrations and models with associations

--- source/app/models/survey.rb
+++ source/app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ActiveRecord::Base
   belongs_to :user
+  has_many :questions
   has_many :survey_surverytakers
   has_many :surveytakers, through: :survey_surverytakers, source: :user
 end

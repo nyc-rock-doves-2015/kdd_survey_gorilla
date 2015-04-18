d35c609b021879daff142886f117bb2fb151aa22
Dan Asselin <dasselin86@gmail.com>
2015-04-17 20:26:10 -0400

    Add #num_of_answers and #percentage_answered methods to Option model

--- source/app/models/option.rb
+++ source/app/models/option.rb
@@ -1,4 +1,12 @@
 class Option < ActiveRecord::Base
   belongs_to :question
   has_many :answers
+
+  def num_of_answers
+    (self.answers.count).to_f
+  end
+
+  def percentage_answered(num_of_takers)
+    ((num_of_answers / num_of_takers) * 100).round
+  end
 end

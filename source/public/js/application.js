2f6a0416866ef021e77508ff5551adcab3660e52
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-18 17:53:37 -0400

    Refactor first ajax call by deleting $target variable

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
   //  ("<input type='text' name='" + name + "' placeholder='question'>");
   // });
   var count = 0
-
-    $("#new_question_button").on('click', function(event){
-    var $event = $(event.target);
+  $("#new_question_button").on('click', function(){
     $.ajax({
       url: '/surveys',
       type: 'POST'
@@ -13,8 +11,12 @@ $(document).ready(function() {
       var $response = $(response);
       count ++;
       $response.children()[0].name = "question[" + count + "]";
-       $('.new_question').last().append($response)
+      $('.new_question').last().append($response)
       // console.log(response)
-    })
-  })
+    });
+  });
+
+  // $("#new_option_button").on('click', function(){
+  //   var
+  // });
 });

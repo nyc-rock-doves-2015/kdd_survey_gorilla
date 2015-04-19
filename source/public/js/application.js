8c73ea1b33358e27b2e22e237063c83f4eb36373
Dan Asselin <dasselin86@gmail.com>
2015-04-19 14:34:57 -0400

    Add second parameter to .on() for correct event bubbling in new question ajax call. Working functionality.

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-  // $('#new_question_button').on('click', function(e){
-  //  ("<input type='text' name='" + name + "' placeholder='question'>");
-  // });
   var count = 0
-  $("#new_survey").on('click', function(event){
+  $("#new_survey").on('click', '#new_question_button', function(event){
     $.ajax({
       url: '/surveys',
       type: 'POST'
@@ -11,28 +8,26 @@ $(document).ready(function() {
       $target = $(event.target);
       console.log($target);
       var $response = $(response);
+      count ++;
+      $response.children()[0].name = "question[" + count + "]";
       $response.insertBefore('#new_question_button');
-      // count ++;
-      // $response.children()[0].name = "question[" + count + "]";
-      // var $closestQuestion = $target.parent().find('.new_question');
-      // $response.appendTo('#new_survey')
     });
   });
 
-  $(".new_question").on('click', function(event){
-    // console.log('you clicked it');
-    $.ajax({
-      url: '/options/new',
-      type: 'GET'
-    }).done(function(response){
-      var $target = $(event.target);
-      // console.log('hi');
-      //add response to new_question div that contained the target
-      var $closestQuestion = $target.closest('.new_question');
-      $closestQuestion.append('hi');
-      // $closestQuestion.append(response);
-      // var $closestOptionButton =
-      // $closestQuestion.before('.new_option_button', response);
-    });
-  });
+  // $(".new_question").on('click', function(event){
+  //   // console.log('you clicked it');
+  //   $.ajax({
+  //     url: '/options/new',
+  //     type: 'GET'
+  //   }).done(function(response){
+  //     var $target = $(event.target);
+  //     // console.log('hi');
+  //     //add response to new_question div that contained the target
+  //     var $closestQuestion = $target.closest('.new_question');
+  //     $closestQuestion.append('hi');
+  //     // $closestQuestion.append(response);
+  //     // var $closestOptionButton =
+  //     // $closestQuestion.before('.new_option_button', response);
+  //   });
+  // });
 });
